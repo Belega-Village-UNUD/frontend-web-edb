d667433d9e44d2c9e98e9436d3a0b236363f9c79
Dicky Umardhani <dicky@alterolab.com>
2024-10-14 00:00:08 +0800

    refactor: change nginx service name

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -14,7 +14,7 @@ fi
 docker service ls | grep "frontend_web_app"
 
 if [ $? -ne 0 ]; then
-  export $(cat .env) > /dev/null 2>&1; docker stack deploy -c ./docker-compose.yml frontend_web
+  export $(cat .env) >  /dev/null 2>&1; docker stack deploy -c ./docker-compose.yml frontend_web
   if [ $? -ne 0 ]; then
       echo "Error in deploying $BRANCH of Frontend Belega Service"
       exit 1
@@ -27,6 +27,21 @@ else
   fi
 fi
 
+docker service ls | grep "ssl_nginx"
+
+if [ $? -ne 0 ]; then
+  export $(cat .env) > /dev/null 2>&1; docker stack deploy -c ./docker/ssl/docker-compose.yml ssl
+  if [ $? -ne 0 ]; then
+      echo "Error in deploying $BRANCH of SSL Belega Service"
+      exit 1
+  fi
+else
+  docker stack deploy -c ./docker/ssl/docker-compose.yml ssl
+  if [ $? -ne 0 ]; then
+      echo "Error in deploying $BRANCH of SSL Belega Service"
+      exit 1
+  fi
+fi
 
 echo "Successfully deploy the image for ghcr.io/belega-village-unud/frontend-web-edb:$COMMIT_SHA on service frontend_web_app"
 
